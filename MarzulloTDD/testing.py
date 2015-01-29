c3e449f97af8a431d19a70818607e8d04fcde2c1
richard <richardlm57@gmail.com>
2015-01-29 15:00:03 -0430

    Modificacion de Marzullo para arreglar la tabla de reservaciones. \nCaso de prueba: probar un estacionamiento de 10 puesto full para con un reservacion en esa hora. Conductor: Patricia

--- MarzulloTDD/testing.py
+++ MarzulloTDD/testing.py
@@ -11,20 +11,21 @@ from marzullo import *
 class testMarzullo(unittest.TestCase):
     
     def testMarzulloExists(self):
-        marzulloAlg([[600,1],[700,-1]],600,800)
+        marzulloAlg([[600,-1],[700,1]],600,800)
         
     def testAddFirstReservation(self):
         self.assertEqual(marzulloAlg([],900,1000),[1,900,1000])
     
     def testAddSecondReservation(self):
-        self.assertEqual(marzulloAlg([[600,1],[700,-1]],900,1000),[1,600,700])
+        self.assertEqual(marzulloAlg([[600,-1],[700,1]],900,1000),[1,600,700])
         
     def testOverlapping(self):
-        self.assertEqual(marzulloAlg([[700,1],[1000,-1],[900,1],[1400,-1]],800,1100),[3,900,1000])
+        self.assertEqual(marzulloAlg([[700,-1],[1000,1],[900,-1],[1400,1]],800,1100),[3,900,1000])
             
     def testStartEqualsEnd(self):
-        self.assertEqual(marzulloAlg([[600,1],[700,-1],[900,1],[1000,-1]],700,800),[1,600,700])
-        
+        self.assertEqual(marzulloAlg([[600,-1],[700,1],[900,-1],[1000,1]],700,800),[1,600,700])
     
+    def testNoSpaceInParkingLot(self):
+        self.assertEqual(marzulloAlg([[700,-1],[900,1]]*10,800,900),[11,800,900])    
     
     
